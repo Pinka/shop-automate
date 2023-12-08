0791247c3b09f7027ce9e72b745ec346d3e96cde
Pinka <mayhem@inbox.lv>
2023-12-08 17:34:42 +0200

    Auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET,
+    // }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ app/components/AppHeader.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export const AppHeader = () => {
+  const { data: session, status } = useSession();
+
+  return (
+    <header className="flex flex-row items-center gap-2 justify-between p-2 border-b">
+      <h1>Shop Automate</h1>
+      <div className="flex items-center gap-2">
+        {session && (
+          <>
+            <p>{session.user?.name}</p>
+            <button onClick={() => signOut()} className="border p-2">
+              Sign out
+            </button>
+          </>
+        )}
+        {!session && status !== "loading" && (
+          <>
+            <p>Not signed in</p>
+            <button onClick={() => signIn()} className="border p-2">
+              Sign in
+            </button>
+          </>
+        )}
+      </div>
+    </header>
+  );
+};
