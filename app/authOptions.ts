d460ca5ab51b315f0786c6ddc92bad8780d4eb06
Pinka <mayhem@inbox.lv>
2023-12-28 17:18:19 +0200

    Route export fix

---
+++ app/authOptions.ts
@@ -0,0 +1,14 @@
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET,
+    // }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
